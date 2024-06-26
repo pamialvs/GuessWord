6472476677f1307d65459524dcd08c9c9e03c750
Pamela <pamelawallescaf@gmail.com>
2024-06-26 17:32:06 -0300

    Novos arquivos adicionados

--- game.c
+++ game.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
 int main()
 {
     char tentativa[6];
-    char palavra[6] = "areio";
+    char palavraSecreta[6];
+    char palavras[1000][6];
+
+    FILE *file = fopen("C:\\users\\pamel\\OneDrive\\Documentos\\GuessWord\\palavras.txt", "r");
+    for(int i = 0; i < 1000; i++){
+        fscanf(file, "%5s", palavras[i]);
+    }
+
+    fclose(file);
+    srand(time(NULL));
+    int randomIndex = rand() % 1000;
+    strcpy(palavraSecreta, palavras[randomIndex]);
 
     for (int j = 0; j < 6; j++){
         printf("====== Digite uma palavra: ");
         scanf("%s", tentativa);
 
-        if (!strcmp(palavra, tentativa) == 1){
+        if (strcmp(palavraSecreta, tentativa) == 0){
             printf("VOCE ACERTOU!!\n");
             return 0;
-            break;
         }else{
             for (int i = 0; i < 5; i++){
-                if (palavra[i] == tentativa[i]){
+                if (palavraSecreta[i] == tentativa[i]){
                     printf("letra %c\nno indice %d\n", tentativa[i], i);
                 }
             }
         }
     }
+    printf("Você não conseguiu, a palavra secreta é: %s", palavraSecreta);
 
     return 0;
 }
